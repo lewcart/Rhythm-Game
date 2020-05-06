80d96bf9657ddf3930d4b32a8e19c5a9206ea209
Reese Norman <reesenorman@outlook.com>
2020-05-06 14:29:12 +1000

    Background, input, menu changes

---
+++ GameV0.5/Assets/Difficulty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Difficulty : MonoBehaviour
+{
+    public void PlayEasy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayNormal()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayHard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+
+}
---
+++ GameV0.5/Assets/Reset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Reset : MonoBehaviour
+{
+    //reloads Game scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
